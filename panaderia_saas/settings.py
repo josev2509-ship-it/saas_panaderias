7d2af54af63015bdec525c8f06c1d08759ed1ca4
Jose_Vicente <josev2529@gmail.com>
2026-05-06 23:56:21 -0400

    Corregir ruta de templates globales

--- panaderia_saas/settings.py
+++ panaderia_saas/settings.py
@@ -60,7 +60,10 @@ ROOT_URLCONF = "panaderia_saas.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "panaderia_saas", "templates")],
+        "DIRS": [
+    os.path.join(BASE_DIR, "templates"),
+    os.path.join(BASE_DIR, "panaderia_saas", "templates"),
+],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
